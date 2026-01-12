78cd807e821dd8476cb6ec12aca6c08f2007d142
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2026-01-12 09:50:15 -0300

    Initial commit

---
+++ src/test/java/ec/carper/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.carper.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
